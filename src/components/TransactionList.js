bf3052b4bc8c8a4de3331ccc92dd64ddc96091aa
Cindy Weiss <cindy.weiss@outlook.it>
2025-05-25 23:46:42 +0200

    Update TransactionList.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -3,7 +3,8 @@ const TransactionList = ({ transactions, onDelete }) => (
     <h3>Movimenti</h3>
     <ul>
       {transactions.map((t) => (
-        <li key={t.id}>
+        <li key={t.id}
+  className={`transaction ${t.amount < 0 ? 'negative' : 'positive'}`}>
           {t.text} ({t.amount} €)
           <button onClick={() => onDelete(t.id)}>X</button>
         </li>
